36190ae8659d4387000fa93b405db9e42a8d35d1
Mutlucan ERVURAL <mutlucan95@hotmail.com>
2024-07-03 20:01:16 +0300

    Added BST Problem

---
+++ Tutorials/30 Days of Code/Binary_Search_Trees.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node{
+    struct Node* left;
+    struct Node* right;
+    int data;
+}Node;
+Node* newNode(int data){
+    Node* node=(Node*)malloc(sizeof(Node));
+    node->left=node->right=NULL;
+    node->data=data;
+    return node;
+}
+
+int getHeight(Node* root){
+    int maxLeft = 0, maxRight = 0;
+    
+    // return "-1" because we count edges not nodes
+    if(root == NULL)
+        return -1;
+
+    //traverse the right subtree
+    maxRight = getHeight(root->right);
+    //traverse the left subtree
+    maxLeft = getHeight(root->left);
+    
+    return 1 + ((maxRight > maxLeft) ? maxRight : maxLeft);
+
+}
+
+Node* insert(Node* root,int data){
+    if(root==NULL)
+        return newNode(data);
+    else{
+        Node* cur;
+        if(data<=root->data){
+            cur=insert(root->left,data);
+            root->left=cur;                
+        }
+        else{
+            cur=insert(root->right,data);
+            root->right=cur;
+        }
+        
+    }
+    return root;
+}
+int main(){
+    Node* root=NULL;
+    int T,data;
+    scanf("%d",&T);
+    while(T-->0){
+        scanf("%d",&data);
+        root=insert(root,data);
+    }
+    int height=getHeight(root);
+    printf("%d",height);
+    return 0;
+    
+}
