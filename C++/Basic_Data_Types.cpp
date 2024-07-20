75aa27e458221c482b2eafb3d540985fbdf73e5f
Mutlucan ERVURAL <mutlucan95@hotmail.com>
2024-07-20 16:15:27 +0300

    Added Basic Data Types Problem

---
+++ C++/Basic_Data_Types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a = 0; long b = 0; char ch; float c = 0; double d = 0;
+    scanf("%d %ld %c %f %lf", &a, &b, &ch, &c, &d);
+    printf("%d\n%ld\n%c\n%f\n%lf", a, b, ch, c, d);
+    return 0;
+}
+
